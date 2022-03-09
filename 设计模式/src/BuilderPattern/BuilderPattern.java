65d268e61ad0a58d776d1441d3008ca48f5499fb
mac air <2033575040@qq.com>
2022-03-09 15:23:59 +0000

    建造者模式

---
+++ 设计模式/src/BuilderPattern/BuilderPattern.java
@@ -0,0 +1,17 @@
+package BuilderPattern;
+
+public class BuilderPattern {
+    public static void main(String[] args) {
+        MealBuilder mb = new MealBuilder();
+
+        Meal vegMeal = mb.prepareVegMeal();
+        System.out.println("Veg Meal");
+        vegMeal.showItems();
+        System.out.println("Total Cost: " + vegMeal.getCost());
+
+        Meal nonVegMeal = mb.prepareNonVegMeal();
+        System.out.println("\n\nNon-Veg Meal");
+        nonVegMeal.showItems();
+        System.out.println("Total Cost: " + nonVegMeal.getCost());
+    }
+}
